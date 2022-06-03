d2a292e38bb324ac2310e9e68166f34854d23e2f
Onyedikachi <onyedikachinwosu@rocketmail.com>
2022-06-03 15:55:31 +0100

    add post handler for creating users

--- signtech_rest_resource/src/Plugin/rest/resource/SignTechResource.php
+++ signtech_rest_resource/src/Plugin/rest/resource/SignTechResource.php
@@ -16,7 +16,8 @@ use Symfony\Component\HttpFoundation\Request;
  *   id = "signtech_resource",
  *   label = @Translation("SignTech Resource"),
  *   uri_paths = {
- *     "canonical" = "/api/1.1"
+ *     "canonical" = "/api/1.1",
+ *     "create" = "/api/1.1"
  *   }
  * )
  */
@@ -50,26 +51,105 @@ class SigntechResource extends ResourceBase {
 
     return new ResourceResponse($response);
   }
-  // public function post(Request $request) {
-  //   $query = \Drupal::request()->query;
-  //   $response = [];
-  //   $params = Json::decode($request->getContent());
-  //   extract($params);
-  //   if($name!='' && $email!=''){
-  //     $response["ServerMsg"]=[
-  //         "your_name" => $name,
-  //         "your_email" => $email,
-  //         "Msg" => "SUCCESS",
-  //         "DisplayMsg" => "Rest message for post"
-  //     ];
-  //   }
-  //   else{
-  //     $response["ServerMsg"]=[
-  //         "Msg" => "Failure",
-  //         "DisplayMsg" => "Rest message for post",
-  //         "DisplayMsg1" => "Name & Email is required"
-  //     ];
-  //   }
-  //   return new ResourceResponse($response);
-  // }
+  public function post(Request $request) {
+    $query = \Drupal::request()->query;
+    $response = [];
+    $params = Json::decode($request->getContent());
+    extract($params);
+
+
+    \Drupal::logger('signtech_rest_resource')->notice('params data',
+        array(
+            '@type' => "test\\",
+            '%title' => "test",
+        ));
+    if($name!='' && $email!=''){
+
+      $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+      $user = User::create();
+
+      // Mandatory.
+      $user->setPassword('password');
+      $user->enforceIsNew();
+      $user->setEmail($email);
+      $user->setUsername($name);
+
+      // Custom Values
+      // $user->set("field_machine_name", 'value');
+
+      // Optional.
+      $user->set('init', 'email');
+      $user->set('langcode', $language);
+      $user->set('preferred_langcode', $language);
+      $user->set('preferred_admin_langcode', $language);
+      // $user->set('setting_name', 'setting_value');
+      $user->addRole('administrator');
+      $user->activate();
+
+      // Save user account.
+      $result = $user->save();
+
+      $messge = $result == 1 ? "User with name $name, and email $email was created successfully": "an error occured while creating user $name";
+      \Drupal::logger('signtech_rest_resource')->notice(json_encode($message));
+
+      $response["ServerMsg"]=[
+          "your_name" => $name,
+          "your_email" => $email,
+          "Msg" => "SUCCESS",
+          "DisplayMsg" => "Rest message for post"
+      ];
+    }
+    else{
+      $response["ServerMsg"]=[
+          "Msg" => "Failure",
+          "DisplayMsg" => "Rest message for post",
+          "DisplayMsg1" => "Name & Email is required"
+      ];
+    }
+    return new ResourceResponse($response);
+  }
 }
+
+
+
+
+// $user = User::create(array(
+//   'uid' => $data[0],
+//   'name' => $data[1],
+//   'pass' => $data[2],
+//   'mail' => $data[3],
+//   'status' => $data[4],
+//   'access' => $data[6],
+//   'login' => $data[7],
+//   'timezone' => $data[8],
+//   'langcode' => $data[9],
+//   'preferred_langcode' => $data[9],
+//   'preferred_admin_langcode' => $data[9],
+//   'init' => $data[10],
+// ));
+// $user->created = $data[5];
+// $roles = explode(",", $data[11]);
+// foreach ($roles as $role) {
+//   switch ($role) {
+//     case 3:
+//       $user->addRole('administrator');
+//       break;
+
+//     case 4:
+//       $user->addRole('contributeur');
+//       break;
+
+//     case 6:
+//       $user->addRole('admin_m2');
+//       break;
+//   }
+// }
+// // Save user account.
+// $user->save();
+
+// $database = \Drupal::database();
+// // Dirty overwrite of the re-hashed hash.
+// $database->merge('users_field_data')
+//   ->fields(['pass' => 'YOUR_D7_HASH_HERE'])
+//   ->keys(array('uid' => $data[0]))
+//   ->execute();
